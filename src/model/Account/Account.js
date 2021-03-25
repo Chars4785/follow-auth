2e3defaac452df044016a5ccc763424e347c2ab5
Chars4785 <9105lgm@naver.com>
2021-03-26 08:31:00 +0900

    user Router 및 user token api 수정

--- src/model/Account/Account.js
+++ src/model/Account/Account.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 import crypto from 'crypto';
 import { ObjectID } from 'mongodb';
-
+import _ from 'lodash';
+const { DEFAULT_PASSWORD } = process.env
 // 고객에 대한 정보
 // 나중에 어떤 교회에 포함인지, 어디 소속인지에 따라서 나눠야 한다.
 const accountSchema = new mongoose.Schema({
@@ -28,25 +29,14 @@ const accountSchema = new mongoose.Schema({
 );
 
 accountSchema.statics.getPasswordHashed = (password) =>{
+    if( _.isUndefined(password) ) password = DEFAULT_PASSWORD;
     return crypto.createHash( 'sha512' ).update( password ).digest( 'base64' );
 }
 
-accountSchema.post( 'findOne', ( document ) => {
-    let message;
-    if ( !document ) {
-        message = '계정정보를 찾을수없습니다.';
-    } 
-    // else if ( document.refs && document.refs.userId && document.deletedAt ) {
-    //     // staff는 탈퇴된계정을 복구할수있다, 고객 계정정보만 걸러야한다.
-    //     message = '이미 탈퇴된 계정입니다.';
-    // }
-    if ( message ) {
-        const error = new Error( message );
-        error.statusCode = 400;
-        error.isIntend = true;
-        throw error;
-    }
-});
+// accountSchema.post( 'findOne', ( document ) => {
+//     if ( !document ) {
+//     } 
+// });
 
 const Account = mongoose.model( 'Account', accountSchema );
 export default Account;
