7a4e3bd4a7dac3faff3401c42c3180a94ffe51d5
Chars4785 <9105lgm@naver.com>
2021-04-26 22:44:27 +0900

    signToken Api - 코드 보기 편하게 변경

--- src/infra/follow-auth.js
+++ src/infra/follow-auth.js
@@ -9,7 +9,12 @@ const { JWT_SECRET_KEY } = process.env;
 const signToken = async ({ data, tokenType, expireIn }) => {
     const TOKEN_EXPIRE_IN = Math.floor( Date.now() / 1000 ) + ( 60 * 60 * 24 );
     const REFRESH_TOKEN_EXPIRE_IN = Math.floor( Date.now() / 1000 ) + ( 60 * 60 * 24 * 90 );
-    const accessToken = await jwt.sign({ data, exp: TOKEN_EXPIRE_IN }, process.env.JWT_SECRET_KEY, { algorithm: TOKEN_ALGORITHM });
+    const accessToken = await jwt.sign({ 
+        data,
+        exp: TOKEN_EXPIRE_IN
+     }, 
+        process.env.JWT_SECRET_KEY, 
+    { algorithm: TOKEN_ALGORITHM });
     const refreshToken = await jwt.sign({ data, exp: REFRESH_TOKEN_EXPIRE_IN }, process.env.JWT_SECRET_KEY, { algorithm: TOKEN_ALGORITHM });
     return {
         tokenType: tokenType || TOKEN_TYPE,
@@ -26,7 +31,8 @@ async function decodeToken(request){
     };
     const token = authorization.replace( 'Bearer ', '' );
     try {
-        return request.decoded = await jwt.verify( token, JWT_SECRET_KEY );
+        request.decoded = await jwt.verify( token, JWT_SECRET_KEY );
+        return request.decode
     } catch ( e ) {
         const decoded = await jwt.decode(token);
         let error;
