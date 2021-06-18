9a72272d48a1243658a0b8a5b0cfa84ad6fd1eeb
Chars4785 <9105lgm@naver.com>
2021-06-18 09:51:29 +0900

    body-parser 변경

--- src/infra/follow-express.js
+++ src/infra/follow-express.js
@@ -13,9 +13,8 @@ const app = express();
 require( 'dotenv' ).config();
 
 app.use(cors())
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(`${SERVICE_PRIFIX}/v1/sign`, signInToken);
 
